d0a2aad0da7005f34dfc523d3c5ec0b334f6cd8e
Alex Meyer <ameyer505@gmail.com>
2024-02-11 21:14:31 +0000

    Removing external Entra ID connection in favor of using built in X++ option

--- Project/D365FOAdminToolkit/D365FOAdminToolkitNET/Models/EntGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace D365FOAdminToolkitNET.Models
-{
-    public class EntGroup
-    {
-        public string Description { get; set; }
-        public string DisplayName { get; set; }
-        public string Id { get; set; }
-    }
-}
